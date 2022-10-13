1b1dce7ee54057e238b97884c6488340f9248031
omar3o3 <otantawy53@gmail.com>
2022-10-12 21:21:10 -0400

    rendering the articles

--- env/blog/views.py
+++ env/blog/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.views import View
 from datetime import datetime
-
+from django.shortcuts import render
 
 # Create your views here.
 class Home(View):
@@ -17,17 +17,19 @@ class Article(View):
     def get(self, request):
         articles = [
             {
-                "title": "python is cool",
+                'title': "python is cool",
                 'category': 'tech',
+                'content': 'blah blah blah blah',
                 'author': 'guido',
                 'creation_date': datetime.now()
             },
             {
-                "title": 'tesla goes bankrupt',
+                'title': 'tesla goes bankrupt',
                 'category': 'auto',
+                'content': 'blah2 blah2 blah2 blah2',
                 'author': 'elon musk',
                 'creation_date': datetime.now()
             }
         ]
 
-        return HttpResponse(articles)
+        return render(request, 'articles.html', {'articles': articles})
